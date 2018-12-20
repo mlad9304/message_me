9d766c86020d49245aeb960da9d227a34c0d0384
rndev80 <rndev80@gmail.com>
2018-12-20 13:55:21 +0100

    Add user and message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Alex", password: "123456")
+User.create(username: "Flynn", password: "123456")
+User.create(username: "Leon", password: "123456")
+User.create(username: "Chris", password: "123456")
+User.create(username: "Marco", password: "123456")
+User.create(username: "John", password: "123456")
