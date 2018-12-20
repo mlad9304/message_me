b8047bc8894455b6873d603b10fd84e9d84e16b5
rndev80 <rndev80@gmail.com>
2018-12-20 14:39:21 +0100

    Enable flash messages display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,7 @@
 
 $(document).ready(function() {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
 });
