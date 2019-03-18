9c32e5e852c7f48de1ffc806d7f9059d206ddf27
rndev80 <rndev80@gmail.com>
2019-03-18 22:50:58 +0100

    Add auto-scrolling to chat window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,16 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function () {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
 $(document).ready(function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
+    scroll_bottom();
 });
